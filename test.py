fb4d9d77338ca76d720124f29c563ff742eb079f
Jakub <jakubszubzda150@gmail.com>
2025-02-19 11:23:23 +0100

    Add additional cube object and improve camera parameters in rendering

--- test.py
+++ test.py
@@ -4,13 +4,15 @@ objects = ObjectsToRender()
 cube1 = Objecs3D(0, 0, 0, "cube", (1, 1, 1))
 cube2 = Objecs3D(2, 0, 0, "cube", (1, 2, 1))
 cube3 = Objecs3D(0, 0, 3, "cube", (1, 1, 2))
+cube4 = Objecs3D(0, 10, 0, "cube", (1, 1, 1))
 
 objects.add(cube1)
 objects.add(cube2)
 objects.add(cube3)
+objects.add(cube4)
 
-print(objects.objects)
+print('There are ', len(objects.objects), " objects being rendered")
 
-camera = Camera(0, -2, 0, 1, (2, 1), 1)
+camera = Camera(0, -2, 0, 1.5, (2, 1.5), 2)
 
 camera.display(objects)
